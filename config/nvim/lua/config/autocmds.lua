6f76ac25e67d2cdc7e1409719fe00a7a8ca6a849
Yan Paing Oo <yanpaingoo008ypo@gmail.com>
2023-11-27 21:09:52 +0630

    Chore(Update): Change to Lazy vim with custom tweaks

---
+++ config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,18 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds hereby
+
+-- Turnoff pastemode when leaving inserts
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+-- Fix conceallevl for json files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
+})
---
+++ config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "fish",
+        "css",
+        "gitignore",
+        "go",
+        "http",
+        "rust",
+        "javascript",
+        "typescript",
+      },
+    },
+  },
+}
